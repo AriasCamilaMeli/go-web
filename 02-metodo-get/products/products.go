2056530d558611ebbdd6eaf771596c95db5cae67
Camila Arias <camila.arias@mercadolibre.com.co>
2025-01-14 11:41:57 -0500

    Configuration Initial

---
+++ 02-metodo-get/products/products.go
@@ -0,0 +1,11 @@
+package products
+
+type Product struct {
+	id           int
+	name         string
+	quantity     int
+	code_value   string
+	is_published bool
+	expiration   string
+	price        float64
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AriasCamilaMeli/go-web
+
+go 1.23.4
