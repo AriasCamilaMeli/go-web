6b78ebaee42cdf0783a4dde9d90f301402098291
Camila Arias <camila.arias@mercadolibre.com.co>
2025-01-14 22:46:46 -0500

    Ejericicios GET

--- 02-metodo-get/main.go
+++ 02-metodo-get/main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/AriasCamilaMeli/go-web/02-metodo-get/products"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+// Controlador para manejar la ruta /ping
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("pong"))
+}
+
 func main() {
 
+	// Crear un nuevo enrutador
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	// Definir rutas y asignar controladores
+	r.Get("/ping", pingHandler)
+	r.Get("/products/{id}", products.GetProductByIDHandler())
+	r.Get("/products", products.GetAllProductsHandler())
+	r.Get("/products/search", products.SearchProductsHandler())
+
+	// Iniciar el servidor
+	fmt.Println("Servidor encendido en el puerto http://localhost:8080")
+	http.ListenAndServe(":8080", r)
 }
--- 02-metodo-get/products/products.go
+++ 02-metodo-get/products/products.go
@@ -1,11 +1,112 @@
 package products
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+)
+
 type Product struct {
-	id           int
-	name         string
-	quantity     int
-	code_value   string
-	is_published bool
-	expiration   string
-	price        float64
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Quantity    int     `json:"quantity"`
+	CodeValue   string  `json:"code_value"`
+	IsPublished bool    `json:"is_published"`
+	Expiration  string  `json:"expiration"`
+	Price       float64 `json:"price"`
+}
+
+func LoadProducts(filename string) ([]Product, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file: %w", err)
+	}
+
+	var products []Product
+	err = json.Unmarshal(data, &products)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal JSON: %w", err)
+	}
+
+	return products, nil
+}
+
+func GetProductByIDHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		products, err := LoadProducts("products.json")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		productID := chi.URLParam(r, "id")
+		id, err := strconv.Atoi(productID)
+		if err != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "{'response':'error'}")
+			return
+		}
+
+		for _, product := range products {
+			if product.ID == id {
+				w.Header().Set("Content-Type", "application/json")
+				json.NewEncoder(w).Encode(product)
+				return
+			}
+		}
+		http.Error(w, "Product not found", http.StatusNotFound)
+	}
+}
+
+func GetAllProductsHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		products, err := LoadProducts("products.json")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(products)
+	}
+}
+
+func SearchProductsHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		products, err := LoadProducts("products.json")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		priceGt := r.URL.Query().Get("priceGt")
+		priceGtInt, err := strconv.Atoi(priceGt)
+		if err != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "{'response':'error'}")
+			return
+		}
+
+		var filteredProducts []Product
+		for _, product := range products {
+			if product.Price > float64(priceGtInt) {
+				filteredProducts = append(filteredProducts, product)
+			}
+		}
+
+		if len(filteredProducts) < 1 {
+			w.Header().Set("Content-Type", "application/json")
+			http.Error(w, "Product not found", http.StatusNotFound)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(filteredProducts)
+	}
 }
